0f0a083e74c5257365e1bece7cc8c30f1f0dfb25
kdiiaz <dev.diiaz@gmail.com>
2021-05-26 12:59:19 -0400

    App.js import route, link, switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './JuanDiaz.jpg';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Check Me Out!
+          Check Me Out! 
         </a>
       </header>
     </div>
